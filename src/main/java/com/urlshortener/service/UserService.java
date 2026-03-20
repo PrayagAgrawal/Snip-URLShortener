21937b4e99099e1d94a1c74feb9f5284259e290b
prayag <prayagagrawal19@gmail.com>
2026-03-21 00:45:06 +0530

    Snip - URL Shortener (Spring Boot)

---
+++ src/main/java/com/urlshortener/service/UserService.java
@@ -0,0 +1,50 @@
+package com.urlshortener.service;
+
+import com.urlshortener.dtos.LoginRequest;
+import com.urlshortener.models.User;
+import com.urlshortener.repository.UserRepository;
+import com.urlshortener.security.jwt.JwtAuthenticationResponse;
+import com.urlshortener.security.jwt.JwtUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtUtils;
+
+    public User registerUser(User user) {
+        System.out.println("--- REGISTER SERVICE: Original password: " + user.getPassword());
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        System.out.println("--- REGISTER SERVICE: ENCODED password: " + encodedPassword); // <-- ADD THIS LINE
+
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse authenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        String jwt = jwtUtils.generateToken(userDetails);
+        return new JwtAuthenticationResponse(jwt);
+    }
+
+    public User findByUsername(String name) {
+        return userRepository.findByUsername(name).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with username: " + name)
+        );
+    }
+}
---
+++ src/test/java/com/urlshortener/snip/SnipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortener.snip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
